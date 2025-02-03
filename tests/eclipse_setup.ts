874c1c71531829dd3cf6723c65bb9b63ce8ea703
Fran <franbbr110@gmail.com>
2025-02-03 16:13:55 -0300

    Test url change

--- tests/eclipse_setup.ts
+++ tests/eclipse_setup.ts
@@ -1,18 +1,20 @@
 import { Page, chromium, Browser } from '@playwright/test';
 import { exec } from 'child_process';
 
-const path = `${__dirname.split('tests')[0]}glsp-theia-integration`
+const path = `${__dirname.split('tests')[0]}/resources/test-workflow/example1.wf`;
+const encodedNewPath = encodeURIComponent(path);
 
 async function eclipse_setup(page: Page) { 
     return new Promise<Page | undefined>((resolve, reject) => {
             try {
-                const result = exec("./setup_glsp_integration.sh");
+                const result = exec("bash ./setup_glsp_integration.sh");
                 result.stdout?.on('data', async (data) => {  
                     // Get token
                     if (data.includes('Launching Eclipse with workspace')) {
-                        await page.waitForTimeout(5000);
+                        await page.waitForTimeout(15000);
                         const pages = await connectToEquoChromiumWithinEclipse();
-                        resolve(pages.find(page => page.url().includes('diagram')));       
+                        const workflowPage = getEquoChromiumPageAndOpenWorkflow(pages);
+                        resolve(workflowPage);       
                     }
                 });
                 result.stderr?.on('data', (data) => {
@@ -26,6 +28,14 @@ async function eclipse_setup(page: Page) {
 }
 
 export default eclipse_setup;
+
+function getEquoChromiumPageAndOpenWorkflow(pages: Page[]) {
+    const page1 = pages.find(page => page.url().includes('diagram'));
+    const url = page1?.url();
+    const updatedUrl = url?.replace(/path=[^&]+/, `path=${encodedNewPath}`);
+    page1?.goto(updatedUrl ? updatedUrl : '');
+    return page1;
+}
 
 async function connectToEquoChromiumWithinEclipse() {
     // Connect to the Equo Chromium browser within Eclipse
